9ce29e88ae700eb8f6a68cb19f732d018a9a89f7
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-10-08 14:25:46 -0400

    Finished version

--- WalkingMan/WalkingMan.Designer.cs
+++ WalkingMan/WalkingMan.Designer.cs
@@ -29,11 +29,17 @@
         private void InitializeComponent()
         {
             this.btnGo = new System.Windows.Forms.Button();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.picMan = new System.Windows.Forms.PictureBox();
+            this.mnuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(302, 217);
+            this.btnGo.Location = new System.Drawing.Point(302, 310);
             this.btnGo.Name = "btnGo";
             this.btnGo.Size = new System.Drawing.Size(75, 23);
             this.btnGo.TabIndex = 0;
@@ -41,21 +47,65 @@
             this.btnGo.UseVisualStyleBackColor = true;
             this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
             // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(389, 24);
+            this.mnuStrip.TabIndex = 1;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // picMan
+            // 
+            this.picMan.Location = new System.Drawing.Point(38, 27);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(312, 223);
+            this.picMan.TabIndex = 2;
+            this.picMan.TabStop = false;
+            // 
             // frmWalkingMan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(389, 261);
+            this.ClientSize = new System.Drawing.Size(389, 399);
+            this.Controls.Add(this.picMan);
             this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
             this.Name = "frmWalkingMan";
             this.Text = "Walking Man";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.PictureBox picMan;
     }
 }
 
--- WalkingMan/WalkingMan.cs
+++ WalkingMan/WalkingMan.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿/*
+Created by Matthew Ho
+Created on 08-10-15
+Created for ICS3U
+3-07
+This program makes  a guy walk in 10 pictures.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+// for pause
+using System.Threading;
 
 namespace WalkingMan
 {
@@ -19,7 +28,63 @@ namespace WalkingMan
 
         private void btnGo_Click(object sender, EventArgs e)
         {
+            int loopCounter = 0;
 
+            do
+            {
+                if (loopCounter == 0)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk2;
+                }
+                else if (loopCounter == 1)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk3;
+                }
+                else if (loopCounter == 2)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk4;
+                }
+                else if (loopCounter == 3)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk5;
+                }
+                else if (loopCounter == 4)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk6;
+                }
+                else if (loopCounter == 5)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk7;
+                }
+                else if (loopCounter == 6)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk8;
+                }
+                else if (loopCounter == 7)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk9;
+                }
+                else if (loopCounter == 8)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk10;
+                }
+                else if (loopCounter == 9)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk1;
+                }
+
+                loopCounter = loopCounter + 1;
+
+                // pause
+                this.Refresh();
+                Thread.Sleep(100);
+
+            } while (loopCounter < 10);
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
